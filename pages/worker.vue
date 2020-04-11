0988ddf7c83792115bd3c681609e1fe776d8e32f
shikachii <shikachii095@gmail.com>
2020-04-12 03:29:20 +0900

    [i18n] workerページを英語に対応

--- pages/worker.vue
+++ pages/worker.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="Worker">
     <h2 class="Worker-Heading">
-      企業の皆様・はたらく皆様へ
+      {{ $t('企業の皆様・はたらく皆様へ') }}
     </h2>
     <div v-for="(item, i) in items" :key="i">
-      <TextCard :title="item.title" :link="item.link" :body="item.body" />
+      <TextCard
+        :title="$t(item.title)"
+        :link="item.link"
+        :body="$t(item.body)"
+      />
     </div>
   </div>
 </template>
 <script lang="ts">
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
 import TextCard from '@/components/TextCard.vue'
-export default {
+
+export default Vue.extend({
   components: {
     TextCard
   },
@@ -67,12 +74,12 @@ export default {
       ]
     }
   },
-  head() {
+  head(): MetaInfo {
     return {
-      title: '企業の皆様・はたらく皆様へ'
+      title: this.$t('企業の皆様・はたらく皆様へ') as string
     }
   }
-}
+})
 </script>
 <style lang="scss">
 .Worker {
